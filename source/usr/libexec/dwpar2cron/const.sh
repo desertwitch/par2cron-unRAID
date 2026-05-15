9aac66f616b34d6291ae727504373b16cbbf3820
desertwitch <24509509+desertwitch@users.noreply.github.com>
2026-05-15 14:24:24 +0200

    changes

--- source/usr/libexec/dwpar2cron/const.sh
+++ source/usr/libexec/dwpar2cron/const.sh
@@ -5,3 +5,12 @@ P2C_LOG_DIR="/var/log/dwpar2cron/log"
 P2C_LOG_FILE="$P2C_LOG_DIR/current"
 P2C_JSON_DIR="/var/log/dwpar2cron/json"
 P2C_JSON_FILE="$P2C_JSON_DIR/current"
+
+if [ -d /mnt/user/system ]; then
+    P2C_CACHE_DIR="/mnt/user/system/par2cron-cache"
+elif mountpoint -q /dev/shm; then
+    P2C_CACHE_DIR="/dev/shm/par2cron-cache"
+else
+    P2C_CACHE_DIR="/tmp/par2cron-cache"
+fi
+mkdir -p "$P2C_CACHE_DIR" || true
